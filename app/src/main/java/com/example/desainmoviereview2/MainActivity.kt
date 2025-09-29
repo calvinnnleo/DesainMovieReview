64134322d92837eb018565dee508019ffe972c44
calvinnnleo <159034319+calvinnnleo@users.noreply.github.com>
2025-09-29 19:58:09 +0700

    Desain ListMovie

--- app/src/main/java/com/example/desainmoviereview2/MainActivity.kt
+++ app/src/main/java/com/example/desainmoviereview2/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.desainmoviereview2
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main) // 👈 INI HARUS activity_main, BUKAN main
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MovieListFragment())
+                .commit()
         }
     }
 }
